d5d9e8ed57fd4bd24d4ad9267d9af01f9c26c423
Andrew Buckingham <abuckingham99@gmail.com>
2022-12-26 17:16:01 -0800

    Initial Commit

---
+++ Piazza/PiazzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiazzaApp.swift
+//  Piazza
+//
+//  Created by Andrew Buckingham on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct PiazzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
